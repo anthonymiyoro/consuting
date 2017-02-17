c86cbaaf41ed7bf655834a7352851fc0baa2d355
anthonymiyoro <amiyoro@gmail.com>
2017-02-17 19:50:37 +0300

    Fixed SQl table error

--- db.py
+++ db.py
@@ -1,34 +1,35 @@
 import sqlite3
 
 # adds add remove and display functipnality
-class Database:
-	# create and store database
-	def __init__(self, dbname="todo.sqlite")
-	    self.dbname = dbname
-	    self.conn = sqlite3.connect(dbname)
 
-	# create a table called items with a text colun called descrtiption
-	def setup(self):
-		stmt = "CREATE TABLE IF NOT EXISTS items (description text)"
-		self.conn.execute(stmt)
-		self.conn.commit()
+
+class Database:
+    # create and store database
+    def __init__(self, dbname="todo.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    # create a table called items with a text colun called descrtiption
+    def setup(self):
+        stmt = "CREATE TABLE IF NOT EXISTS items (description text)"
+        self.conn.execute(stmt)
+        self.conn.commit()
 
      # adds item text to our table
-	def add_item(self, item_text):
-		stmt = "INSERT INTO items(desctiptions) VALUES (?)"
-		args = (item_text, )
-		self.conn.execute(stmt, args)
-		self.conn.commit()
+    def add_item(self, item_text):
+        stmt = "INSERT INTO items(description) VALUES (?)"
+        args = (item_text, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
 
      # remove item_text from datatbase
-	def delete_item(self, item_text):
-		stmt = "DELETE FROM items WHERE description = (?)"
-	    args = (item_text, )
-	    self.conn.execute(stmt, args)
-		self.conn.commit()
+    def delete_item(self, item_text):
+        stmt = "DELETE FROM items WHERE description = (?)"
+        args = (item_text, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
 
-	# return a list of all the items in the database
-	def get_items(self):
-		stmt = "SELECT description FROM items"
-		return [x[0] for x in self.conn.execute(stmt)]
-
+    # return a list of all the items in the database
+    def get_items(self):
+        stmt = "SELECT description FROM items"
+        return [x[0] for x in self.conn.execute(stmt)]
