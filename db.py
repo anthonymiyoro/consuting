e258b37e0c9dc7925366e020828a79097d1e705d
anthonymiyoro <amiyoro@gmail.com>
2017-02-18 00:37:43 +0300

    Added indices

--- db.py
+++ db.py
@@ -12,11 +12,14 @@ class Database:
     # create a table called items with a text colun called descrtiption
     # and an ower column
     def setup(self):
-    	print("creating table")
-        stmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
-        self.conn.execute(stmt)
+        print("creating table")
+        tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
+        itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)"
+        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+        self.conn.execute(tblstmt)
+        self.conn.execute(itemidx)
+        self.conn.execute(ownidx)
         self.conn.commit()
-
 
      # adds item text and owner to our table
     def add_item(self, item_text, owner):
@@ -25,14 +28,12 @@ class Database:
         self.conn.execute(stmt, args)
         self.conn.commit()
 
-
      # remove item_text from database
     def delete_item(self, item_text, owner):
         stmt = "DELETE FROM items WHERE description = (?) AND owner = (?)"
         args = (item_text, owner)
         self.conn.execute(stmt, args)
         self.conn.commit()
-
 
     # return a list of all the items in the database depending on owner
     def get_items(self, owner):
